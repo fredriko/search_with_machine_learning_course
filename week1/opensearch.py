ffcb10cfc5d85ac5ae2859512d93a565104ba376
Fredrik Olsson <fredrik.olsson@gmail.com>
2022-02-18 16:36:18 +0000

    Halfway through the assignment for week 1

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -4,7 +4,22 @@ from opensearchpy import OpenSearch
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
     if 'opensearch' not in g:
+
+        host = "localhost"
+        port = 9200 
+        auth = ("admin", "admin")
+
         # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
-        g.opensearch = None
+        client = OpenSearch(
+            hosts=[{'host': host, 'port': port}],
+            http_compress=True, 
+            http_auth=auth,
+            use_ssl=True,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False
+        )
+
+        g.opensearch = client
 
     return g.opensearch
--- week1/search.py
+++ week1/search.py
@@ -10,10 +10,12 @@ from week1.opensearch import get_opensearch
 bp = Blueprint('search', __name__, url_prefix='/search')
 
 
-# Process the filters requested by the user and return a tuple that is appropriate for use in: the query, URLs displaying the filter and the display of the applied filters
+# Process the filters requested by the user and return a tuple that is appropriate for use in: the query, URLs displaying the filter and the display 
+# of the applied filters
 # filters -- convert the URL GET structure into an OpenSearch filter query
 # display_filters -- return an array of filters that are applied that is appropriate for display
-# applied_filters -- return a String that is appropriate for inclusion in a URL as part of a query string.  This is basically the same as the input query string
+# applied_filters -- return a String that is appropriate for inclusion in a URL as part of a query string.  This is basically the same as the 
+# input query string
 def process_filters(filters_input):
     # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
     filters = []
@@ -23,15 +25,35 @@ def process_filters(filters_input):
         type = request.args.get(filter + ".type")
         display_name = request.args.get(filter + ".displayName", filter)
         #
-        # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in aggregations.jinja2
+        # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in 
+        # aggregations.jinja2
         applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
                                                                                  display_name)
         #TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
-        # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by aggregations.jinja2 (and any other links that would execute a search.)
+        # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by 
+        # aggregations.jinja2 (and any other links that would execute a search.)
         if type == "range":
-            pass
+            _from = request.args.get(f"{filter}.from")
+            _to = request.args.get(f"{filter}.to")
+            _range = {
+                "gte": _from if _from else "*",
+                "lt": _to if _to else "*"
+            }
+            _filter = {"range": {filter: _range}}
+            filters.append(_filter)
+            display_filters.append(f"{display_name}: {_range['gte']} to {_range['lt']}")
+            applied_filters = f"{applied_filters}&{filter}.from={_range['gte']}&{filter}.to={_range['lt']}"
         elif type == "terms":
-            pass #TODO: IMPLEMENT
+            _field = request.args.get(f"{filter}.fieldName", filter)
+            _key = request.args.get(f"{filter}.key")
+            _filter = {
+                "term": {
+                    _field: _key
+                }
+            }
+            filters.append(_filter)
+            display_filters.append(f"{display_name}: {_key}")
+            applied_filters = f"{applied_filters}&{filter}.fieldName={_field}&{filter}.key={_key}"
     print("Filters: {}".format(filters))
 
     return filters, display_filters, applied_filters
@@ -74,8 +96,7 @@ def query():
         query_obj = create_query("*", [], sort, sortDir)
 
     print("query obj: {}".format(query_obj))
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
-    # Postprocess results here if you so desire
+    response = opensearch.search(query_obj)
 
     #print(response)
     if error is None:
@@ -87,14 +108,59 @@ def query():
 
 
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
-    print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
+    print("** In create query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
     query_obj = {
         'size': 10,
+        "sort": [
+            {
+                sort: {
+                    "order": sortDir
+                    }
+            }
+        ],
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+            "bool": {
+                "should": [
+                    {
+                        "multi_match": {
+                            "query": user_query,
+                            "fields" : ["name^2", "shortDescription", "longDescription"]
+                        }
+                    }
+                ],
+                "filter": filters
+            },
         },
         "aggs": {
-            #TODO: FILL ME IN
-        }
+            "department": {
+                "terms": {
+                    "field": "department.keyword",
+                    "min_doc_count": 1
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image"
+                }
+            },
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        {"key": "$", "to": 100},
+                        {"key": "$$", "from": 100, "to": 200},
+                        {"key": "$$$", "from": 200, "to": 300},
+                        {"key": "$$$$", "from": 300, "to": 400},
+                        {"key": "$$$$$", "from": 400, "to": 500},
+                        {"key": "$$$$$$", "from": 500},
+                    ]
+                },
+                "aggs": {
+                    "price_stats": {
+                        "stats": {"field": "regularPrice"}
+                    }
+                }
+            }    
+        },
     }
     return query_obj
